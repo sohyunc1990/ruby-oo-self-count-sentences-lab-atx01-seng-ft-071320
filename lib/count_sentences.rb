976355ea94b80db5d000cc16fae8a0d29e90b237
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-15 21:40:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,8 +3,9 @@ require 'pry'
 class String
 
   def sentence?
-    "Hi, my name is Sophie".end_with?(".")
-    return
+    if self.end_with?(".")
+      return true
+    end
   end
 
   def question?
