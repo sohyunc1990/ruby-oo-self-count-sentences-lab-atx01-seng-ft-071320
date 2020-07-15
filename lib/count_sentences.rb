8f84045cf913b54dac5b4e5c0b104c33d24c3c75
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-15 22:50:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,8 +27,8 @@ class String
   end
 
   def count_sentences
-    self.split(/(?<=[?.!])\s*/).count
-    
+    split_sentence = self.split(/(?<=[?.!])\s*/)
+    return split_sentence.count
   end
   
 end
